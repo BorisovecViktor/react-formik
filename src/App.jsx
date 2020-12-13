3cb4c74a87f969ccf5869b0c8285926f73f7b245
Viktor Borisovets <viktorborisovets@gmail.com>
2020-12-13 14:21:02 +0200

    simple form has been added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,64 @@
 import React from 'react';
+import {Formik, Form, Field, ErrorMessage} from 'formik';
+import * as yup from 'yup';
 import './App.scss';
 
 // import * as store from './store';
 
+const initialValues = {
+  name: 'Viktor',
+  email: '',
+  channel: ''
+}
+
+const validationSchema = yup.object({
+  name: yup.string().required('Required'),
+  email: yup.string().email('Invalid email format').required('Required'),
+  channel: yup.string().required('Required')
+})
+
+const onSubmit = values => {
+  console.log('Form data', values);
+}
+
 const App = () => {
   return (
-    <div className="app">
-      <h1>hi there formik add +</h1>
-    </div>
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={onSubmit}
+    >
+      <Form>
+        <div className='form-control'>
+          <label htmlFor='name'>Name</label>
+          <Field
+            type='text'
+            id='name'
+            name='name'
+          />
+          <ErrorMessage name='name' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='email'>Email</label>
+          <Field
+            type='email'
+            id='email'
+            name='email'
+          />
+          <ErrorMessage name='email' />
+        </div>
+        <div className='form-control'>
+          <label htmlFor='channel'>Channel</label>
+          <Field
+            type='text'
+            id='channel'
+            name='channel'
+          />
+          <ErrorMessage name='channel' />
+        </div>
+        <button type='submit'>Send</button>
+      </Form>
+    </Formik>
   );
 };
 
